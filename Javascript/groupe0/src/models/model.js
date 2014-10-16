f2afc2dd006910d0562adb3d1151530aa9f8fcec
tperroin <thibault.perroin@gmail.com>
2014-10-16 11:51:17 +0200

    Code update

--- Javascript/groupe0/src/models/model.js
+++ Javascript/groupe0/src/models/model.js
@@ -1,51 +1,53 @@
 function Model() {
 
-  var 
-    model = this,
-    money = 40,
-
-  model.Land = function () {
-    var 
-      land           = this, 
-      stock          = 4,
-      sellingPrice   = 12,
-      waterDays      = 0,
-      waterlessDays  = 0,
-      // status : 
-      //  empty : failed 
-      //  grow  : water
-      //  ready : to harvest
-      state = 'grow';  
-
-    // Rupture 
-    setInterval(function () {
-      land.waterDays += 1;
-    }, 2000);
-
-    // failing
-    setInterval(function () {
-      land.waterlessDays += 1;
-    }, 2000);
-  }
-
-  model.init = function () {
     var
-     land1 = new Land(),
-     land2 = new Land(),
-     land3 = new Land();
-  }
+        model = this,
+        money = 40;
 
-  model.water = function (land) {
+    model.Land = function () {
+        var
+            land = this,
+            stock = 4,
+            sellingPrice = 12,
+            waterDays = 0,
+            waterlessDays = 0,
+        // status :
+        //  empty : failed
+        //  grow  : water
+        //  ready : to harvest
+            state = 'grow';
 
-  }
+        // Rupture
+        setInterval(function () {
+            land.waterDays += 1;
+        }, 2000);
 
-  model.sell = function (land) {
+        // failing
+        setInterval(function () {
+            land.waterlessDays += 1;
+        }, 2000);
+    }
 
-  }
+    model.init = function () {
+        var
+            land1 = new Land(),
+            land2 = new Land(),
+            land3 = new Land();
+    };
 
-  model.harvest = function (land) {
+    model.water = function (land) {
 
-  }
+    };
+
+    model.sell = function (land) {
+
+    };
+
+    model.harvest = function (land) {
+
+    };
+}
+
 
 
 
