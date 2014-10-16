9a22a44913342722034305ab20ef5e74a490971e
imie <imie@imie-dev-debian>
2014-10-16 09:30:41 +0200

    create mvc structure

---
+++ Javascript/groupe0/src/launcher.js
@@ -0,0 +1,11 @@
+function launcher () {
+
+  /*
+   * Object instanciation for application initialisation
+   *
+   */
+  model       = new Model();
+  view        = new View();
+  controller  = new Controller();
+
+}
